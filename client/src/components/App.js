a90411510dbf287386a420b0924690ad2360d8eb
cremento <iit2019216@iiita.ac.in>
2021-03-24 12:07:35 +0530

    added navbar and styling modals

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
-import React, {useState, createContext, useEffect} from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Settings from './Settings';
 import LoginModal from './LoginModal';
@@ -10,58 +10,103 @@ import CoolerPage from './CoolerPage';
 import axios from 'axios';
 import SERVER_URL from '../utils/constants';
 
+// changes
+import '../assets/vendor/bootstrap/css/bootstrap.min.css';
+import '../assets/vendor/animate.css/animate.min.css';
+import '../assets/vendor/icofont/icofont.min.css';
+import '../assets/vendor/boxicons/css/boxicons.min.css';
+import '../assets/vendor/venobox/venobox.css';
+import '../assets/vendor/owl.carousel/assets/owl.carousel.min.css';
+import '../assets/vendor/aos/aos.css';
+import '../assets/css/style.css';
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+import { CardImg } from 'react-bootstrap';
+// changes end 
+
 const loggedInContext = createContext([Boolean])
 
-function App(){
+function App() {
 	// const[token, setToken] = useState(false)
 	// const {token, setToken} = useToken(false); //custom hook
 	// console.log('tokenfirst', token)
 
 	const [loggedIn, setLoggedIn] = useState(false);
 
-	const getLoggedInStatus = () =>{
+	const getLoggedInStatus = () => {
 		return axios.post(`${SERVER_URL}/loggedin`)
 	}
-	
+
 	useEffect(() => {
-		async function loggedInStatus(){
+		async function loggedInStatus() {
 			const status = getLoggedInStatus();
 
 			setLoggedIn(status);
 		}
 		loggedInStatus();
 		console.log('logged in status', loggedIn);
-	},[])
+	}, [])
 
 	return (
 		<loggedInContext.Provider value={loggedIn}>
-		<div className = "wholeApp">
-			<h1 style={{'backgroundColor': '#004', 'color': 'white', 'margin': '0', 'padding':'0.5rem 0 1rem 2rem'}}>DrinksapHe - your cooler maintenance dashboard</h1>
-			<hr style={{'backgroundColor': 'white', 'color': 'white', 'margin': '0'}}></hr>
 			{/* <button onClick = {setLoggedIn(false)} >log out</button> */}
 			{/* {loggedIn ? <p>currently logged in</p> : <p>not logged in</p>} */}
-			<BrowserRouter>
-				<Switch>
-					<Route exact path = '/'>
-						<Dashboard />
-					</Route>
-					<Route exact path = '/login'>
-						<LoginModal />
-					</Route>
-					<Route exact path = '/register'>
-						<Register />
-					</Route>
-					<Route exact path = '/settings'>
-						<Settings />
-					</Route>
-					<Route exact path = '/cooler/:id'>
-						<CoolerPage />
-					</Route>
-					
+			
+			<div className="wholeApp">
+				{/* <h1>App Home</h1> */}
+			<h1 style={{'backgroundColor': '#004', 'color': 'white', 'margin': '0', 'padding':'0.5rem 0 1rem 2rem'}}>DrinksapHe - your cooler maintenance dashboard</h1>
+			<hr style={{'backgroundColor': 'white', 'color': 'white', 'margin': '0'}}></hr>
+				{/* <button onClick = {setLoggedIn(false)} >log out</button> */}
+				{/* {loggedIn ? <p>currently logged in</p> : <p>not logged in</p>} */}
 
-				</Switch>
-			</BrowserRouter>
-		</div>
+				{/* changes start */}
+				<header className="fixed-top header-transparent">
+					<div>
+					
+						<Navbar bg="light" expand="lg" >
+							{/* <Navbar.Brand href="#home"></Navbar.Brand> */}
+							<Navbar.Toggle aria-controls="basic-navbar-nav" />
+							<Navbar.Collapse id="basic-navbar-nav">
+								<Nav className="mr-auto">				
+									<Nav.Link href="" className = "active">Home</Nav.Link>
+									<Nav.Link href="login">Login</Nav.Link>
+									<Nav.Link href="register">Register</Nav.Link>
+									<NavDropdown title="Settings" id="basic-nav-dropdown">
+										<NavDropdown.Item href="#action/3.1">Password Change</NavDropdown.Item>
+										<NavDropdown.Item href="#action/3.2">Set pH limit</NavDropdown.Item>
+										<NavDropdown.Item href="#action/3.3">Set alert time</NavDropdown.Item>
+										<NavDropdown.Divider />
+										<NavDropdown.Item href="#action/3.4">Logged out</NavDropdown.Item>
+									</NavDropdown>
+									<Nav.Link href="#">About Us</Nav.Link>
+								</Nav>
+							</Navbar.Collapse>
+						</Navbar>
+					</div>
+				</header>
+
+				{/* changes end */}
+				<BrowserRouter>
+					<Switch>
+						<Route exact path='/'>
+							<Dashboard />
+						</Route>
+						<Route exact path='/login'>
+							<LoginModal />
+						</Route>
+						<Route exact path='/register'>
+							<Register />
+						</Route>
+						<Route exact path='/settings'>
+							<Settings />
+						</Route>
+						<Route exact path='/cooler/:id'>
+							<CoolerPage />
+						</Route>
+
+
+					</Switch>
+				</BrowserRouter>
+			</div>
 		</loggedInContext.Provider>
 	);
 }
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,24 +2,27 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import {Button, Modal} from 'react-bootstrap'
 import Select from 'react-select';
+
 import SERVER_URL from '../utils/constants';
 
 import CoolerCard from './CoolerCard';
 
 import querystring from 'querystring';
+import '../styles/dashboard.css';
+import '../assets/img/campus.jpg';
 
 export default function Dashboard() {
     // const[range, updateRange] = useState({low: 5, high: 5})
-    const[lowRange, setLowRange] = useState();      //not sure if i should default to 7 or no
-    const[highRange, setHighRange] = useState();
+    const [lowRange, setLowRange] = useState();      //not sure if i should default to 7 or no
+    const [highRange, setHighRange] = useState();
 
     //all coolers from db
     const [coolers, updateCoolers] = useState({});
     const [coolerNames, setCoolerNames] = useState([]);
 
     //range form values
-    const[formLow, setFormLow] = useState(7);
-    const[formHigh, setFormHigh] = useState(7);
+    const [formLow, setFormLow] = useState(7);
+    const [formHigh, setFormHigh] = useState(7);
 
     //addCooler form values
     const [coolerName, setCoolerName] = useState();
@@ -74,7 +77,7 @@ export default function Dashboard() {
                 // updateRange(prevRange => {
                 //     return {...prevRange, low:2.33}
                 // });
-                console.log('range state: ',lowRange, highRange);
+                console.log('range state: ', lowRange, highRange);
             }))
             .catch(err => console.log('could not get range in frontend', err))
     }
@@ -82,36 +85,36 @@ export default function Dashboard() {
     //on render, get updated list of coolers from db
     const getCoolers = () => {
         axios.get(`${SERVER_URL}/dashboard/getCoolers`)
-        .then((res => {
-            console.log('res in frontend', res.data);
-            updateCoolers(res.data);
-            console.log('get coolers and save in obj', coolers);
-            // updateCoolers(curr => [...curr, coolersList])
-            
-        }))
-        .catch(err => console.log('could not get coolers in frontend', err))
+            .then((res => {
+                console.log('res in frontend', res.data);
+                updateCoolers(res.data);
+                console.log('get coolers and save in obj', coolers);
+                // updateCoolers(curr => [...curr, coolersList])
+
+            }))
+            .catch(err => console.log('could not get coolers in frontend', err))
     }
 
     //if form is submitted properly, send updated range to backend
     const updateRange = () => {
         axios
-            .post(`${SERVER_URL}/dashboard/range`, querystring.stringify({low: formLow, high: formHigh})
-            , {
-                headers: {
-              'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-            },
-            credentials: 'include',
-            withCredentials: true
-            }
+            .post(`${SERVER_URL}/dashboard/range`, querystring.stringify({ low: formLow, high: formHigh })
+                , {
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                    },
+                    credentials: 'include',
+                    withCredentials: true
+                }
             )
             .then(res => {
-                if(res.status === 200){
+                if (res.status === 200) {
                     // localStorage.setItem('username', res.data.username);
                     console.log("frontend knows range is updated, data:", res.data);
                     // res.redirect('/login');
                     // history.push('/login')
                 }
-                console.log('range response:',res)
+                console.log('range response:', res)
             })
             .catch(err => console.log('range error', err))
     }
@@ -128,22 +131,22 @@ export default function Dashboard() {
         event.preventDefault()
         console.log('cooler form stuff', coolerName, coolerLoc);
 
-        if(!coolerName || !coolerLoc){
+        if (!coolerName || !coolerLoc) {
             alert('all values must be filled');
         }
         else {
             axios
-                .post(`${SERVER_URL}/dashboard/addCooler`, querystring.stringify({coolerName: coolerName, location: coolerLoc})
-                , {
-                    headers: {
-                'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-                },
-                credentials: 'include',
-                withCredentials: true
-                }
+                .post(`${SERVER_URL}/dashboard/addCooler`, querystring.stringify({ coolerName: coolerName, location: coolerLoc })
+                    , {
+                        headers: {
+                            'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                        },
+                        credentials: 'include',
+                        withCredentials: true
+                    }
                 )
                 .then(res => {
-                    if(res.status === 200){
+                    if (res.status === 200) {
                         // localStorage.setItem('username', res.data.username);
                         console.log("frontend knows cooler is added, data:", res.data);
 
@@ -153,7 +156,7 @@ export default function Dashboard() {
                         handleCloseAddCooler()
                         // history.push('/login')
                     }
-                    console.log('addCooler response:',res)
+                    console.log('addCooler response:', res)
                 })
                 .catch(err => console.log('addCooler error', err))
         }
@@ -192,11 +195,11 @@ export default function Dashboard() {
     //range edit form submit actions
     const doUpdateRange = event => {
         event.preventDefault()
-        console.log('range form stuff: ',formLow, formHigh)
+        console.log('range form stuff: ', formLow, formHigh)
 
         rangeUpdateErr = false;
 
-        if(formLow > formHigh){
+        if (formLow > formHigh) {
             console.log('low>high')
             alert('low value should be less than high value');
             // rangeUpdateErr.push('low value should be less than high value')
@@ -204,7 +207,7 @@ export default function Dashboard() {
         }
         // else rangeUpdateErr = false;
 
-        if(formLow > 14 || formLow < 0 || formHigh > 14 || formHigh < 0){
+        if (formLow > 14 || formLow < 0 || formHigh > 14 || formHigh < 0) {
             console.log('bad value')
             alert('range values should be between 0 and 14')
             // rangeUpdateErr.push('range values should be between 0 and 14')
@@ -212,8 +215,8 @@ export default function Dashboard() {
         }
         // else rangeUpdateErr = false;
 
-        if(rangeUpdateErr == false){
-            console.log('errors?',rangeUpdateErr)
+        if (rangeUpdateErr == false) {
+            console.log('errors?', rangeUpdateErr)
 
             updateRange();
             setLowRange(formLow)
@@ -223,79 +226,144 @@ export default function Dashboard() {
             // updateRange({low: formLow, high: formHigh})
             // console.log(range);
         }
-        
+
     }
 
     const renderCoolersList = () => {
         // console.log('len',drinksaphe.coolers.length);
-        if(coolers.list !== undefined){
+        if (coolers.list !== undefined) {
             // console.log(drinksaphe._id)
             // return <p>{drinksaphe._id}</p>
-            return coolers.list.map(cooler => {return <CoolerCard deets = {cooler} low={lowRange} high={highRange} />})
+            return coolers.list.map(cooler => { return <CoolerCard deets={cooler} low={lowRange} high={highRange} /> })
         }
-        
+
         else return <p>coolers not loading</p>
     }
 
-    return(
-        <div style={{'minHeight': '100vh','backgroundColor': '#004', 'color': 'white','display': 'flex', 'flexDirection': 'column','alignItems': 'center', 'paddingTop': '1rem'}}>
-            <h2>valid pH range:</h2>
-            <h1 style={{'fontSize': '6rem'}}>{lowRange} - {highRange}</h1>
-            
-            <div style={{'display': 'flex', 'flexDirection': 'row'}}>
-                <Button style={{'margin':'0.5rem', 'color': 'black', 'backgroundColor': '#EEF'}} variant="primary" onClick={handleShowRange}>
-                    Update valid range
+
+   
+
+    return (
+        <div>
+            {/* <header class = "header">
+                <div id = "campus">
+                {/* <img src="../assets/img/campus.jpg" alt="campus image" /> */}
+                {/* <img src = {campus} alt = "campus image" /> */}
+                {/* </div> */}
+            {/* //  </header> */} 
+
+
+            {/* important code */}
+
+            <div class="body">
+                <h2>Valid pH Range:</h2>
+                <h1 style={{ 'fontSize': '6rem' }}>{lowRange} - {highRange}</h1>
+
+                <div style={{ 'display': 'flex', 'flexDirection': 'row' }}>
+                    <Button class="btn" variant="primary" onClick={handleShowRange}>
+                        Update valid range
+
                 </Button>
-                <Button style={{'margin':'0.5rem', 'color': 'black', 'backgroundColor': '#EEF'}} variant="primary" onClick={handleShowAddCooler}>
-                    Add new cooler
+                    <Button className="btn" variant="primary" onClick={handleShowAddCooler}>
+                        Add new cooler
                 </Button>
-                <Button style={{'margin':'0.5rem', 'color': 'black', 'backgroundColor': '#EEF'}} variant="primary" onClick={handleShowDeleteCooler}>
+
+                <Button className="btn" variant="primary" onClick={handleShowDeleteCooler}>
                     Delete a cooler
                 </Button>
-            </div>
-            
+
+                </div>
 
 
-            <div style={{'display': 'flex', 'flexFlow': 'row wrap', 'justifyContent': 'center'}}>
-                {renderCoolersList()}
-            </div>
-            
 
-            
-            
-            {/* add new cooler modal */}
-            <Modal show={showAddCoolerModal} onHide={handleCloseAddCooler}>
-                <Modal.Header closeButton>
-                <Modal.Title>Add A New Cooler</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>Enter details of new cooler</p>
-                    <form onSubmit = {addCooler}>
-                        <label>
-                            <p>cooler name</p>
-                            <input 
-                                type="text" 
-                                placeholder="for ex: cooler1"
-                                onChange = {event => setCoolerName(event.target.value)}
-                            />
-                        </label>
-                        <label>
-                            <p>cooler location</p>
-                            <input 
-                                type="text" 
-                                placeholder="for ex: in front of room 5448"
-                                onChange = {event => setCoolerLoc(event.target.value)}
-                            />
-                        </label>
-                        <div>
-                            <button type="submit">Add cooler!</button>
-                        </div>
-                    </form>
-                    {/* <p>{rangeUpdateErr[0]}</p> */}
-                    {/* {rangeUpdateErr.length == 0 && <p>no errors</p>}
+                <div style={{ 'display': 'flex', 'flexFlow': 'row wrap', 'justifyContent': 'center' }}>
+                    {renderCoolersList()}
+                </div>
+
+
+                {/* add new cooler modal */}
+                <Modal show={showAddCoolerModal} onHide={handleCloseAddCooler}>
+                    <Modal.Header closeButton style={{ 'background': '#00cccc' }}>
+                        <Modal.Title>Add Cooler Details</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body class="editPop">
+                        <p style={{ 'margin-left': '142px' }}>Enter details of new cooler</p>
+                        <form onSubmit={addCooler}>
+                            <label>
+                                <p class="modalBody">Cooler Name</p>
+                                <div class="modalBody">
+                                    <input
+                                        type="text"
+                                        placeholder="for ex: cooler1"
+                                        onChange={event => setCoolerName(event.target.value)}
+                                        class="textInput"
+                                    />
+                                </div>
+                            </label>
+                            <label>
+                                <p class="modalBody">Cooler Location</p>
+                                <div class="modalBody">
+                                    <input
+                                        type="text"
+                                        placeholder="for ex: in front of room 5448"
+                                        onChange={event => setCoolerLoc(event.target.value)}
+                                        class="textInput"
+                                    />
+                                </div>
+                            </label>
+                            <div class=" btn subBtn">
+                                <button type="submit" class="submitBtn btn1">Add cooler!</button>
+                            </div>
+                        </form>
+                        {/* <p>{rangeUpdateErr[0]}</p> */}
+                        {/* {rangeUpdateErr.length == 0 && <p>no errors</p>}
                     {rangeUpdateErr.length > 0 && rangeUpdateErr.map(al => {console.log('fafaf'); return <p>{al}</p>;})} */}
-                </Modal.Body>
-            </Modal>
+                    </Modal.Body>
+                </Modal>
+
+                {/* range modal */}
+                <Modal show={showRangeModal} onHide={handleCloseRange}>
+                    <Modal.Header closeButton style={{ 'background': '#00cccc' }}>
+                        <Modal.Title>Edit Range</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body class="editPop">
+                        <p style={{ 'margin-left': '111px' }}>Type the updated values of the range</p>
+                        <form onSubmit={doUpdateRange}>
+                            <label>
+                                <p class="modalBody">Low</p>
+                                <div class="modalBody">
+                                    <input
+                                        type="text"
+                                        placeholder="for ex: 6.9"
+                                        onChange={event => setFormLow(event.target.value)}
+                                        class="textInput"
+                                    />
+                                </div>
+                            </label>
+                            <label>
+                                <p class="modalBody">High</p>
+                                <div class="modalBody">
+                                    <input
+                                        type="text"
+                                        placeholder="for ex: 6.9"
+                                        onChange={event => setFormHigh(event.target.value)}
+                                        class="textInput"
+                                    />
+                                </div>
+                            </label>
+                            <div class="btn subBtn">
+                                <button type="submit" class="submitBtn btn1">Update!</button>
+                            </div>
+                        </form>
+                        {/* <p>{rangeUpdateErr[0]}</p> */}
+                        {/* {rangeUpdateErr.length == 0 && <p>no errors</p>}
+                    {rangeUpdateErr.length > 0 && rangeUpdateErr.map(al => {console.log('fafaf'); return <p>{al}</p>;})} */}
+                    </Modal.Body>
+                </Modal>
+
+            </div>
+
+           {/* delete modal  */}
 
             <Modal show={showDeleteCoolerModal} onHide={handleCloseDeleteCooler}>
                 <Modal.Header closeButton>
@@ -316,39 +384,10 @@ export default function Dashboard() {
                 </Modal.Body>
             </Modal>
 
-            {/* range modal */}
-            <Modal show={showRangeModal} onHide={handleCloseRange}>
-                <Modal.Header closeButton>
-                <Modal.Title>Edit Range</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>Type the updated values of the range</p>
-                    <form onSubmit = {doUpdateRange}>
-                        <label>
-                            <p>low</p>
-                            <input 
-                                type="text" 
-                                placeholder="for ex: 6.9"
-                                onChange = {event => setFormLow(event.target.value)}
-                            />
-                        </label>
-                        <label>
-                            <p>high</p>
-                            <input 
-                                type="text" 
-                                placeholder="for ex: 6.9"
-                                onChange = {event => setFormHigh(event.target.value)}
-                            />
-                        </label>
-                        <div>
-                            <button type="submit">update!</button>
-                        </div>
-                    </form>
-                    {/* <p>{rangeUpdateErr[0]}</p> */}
-                    {/* {rangeUpdateErr.length == 0 && <p>no errors</p>}
-                    {rangeUpdateErr.length > 0 && rangeUpdateErr.map(al => {console.log('fafaf'); return <p>{al}</p>;})} */}
-                </Modal.Body>
-            </Modal>
+           
+                    <footer id="footer">
+        
+            </footer>
         </div>
     )
 }
