529143bf1e97e84db40e55e4cee80853b014227f
cremento <iit2019216@iiita.ac.in>
2021-04-05 12:56:54 +0530

    styled cooler page and profile apge

--- client/src/components/CoolerCard.js
+++ client/src/components/CoolerCard.js
@@ -5,6 +5,7 @@ import {useHistory} from 'react-router';
 
 import SERVER_URL from '../utils/constants';
 import querystring from 'querystring';
+import '../styles/coolerCard.css';
 
 export default function CoolerCard({deets, low, high}){
     const history = useHistory();
@@ -77,17 +78,17 @@ export default function CoolerCard({deets, low, high}){
 
     return( 
         <div>
-            <Card style={{'margin':'0.5rem','alignItems': 'center', 'width': '18rem', 'minHeight': '15rem', 'borderWidth': '3px', 'borderColor': cardColor, 'color': 'white', 'backgroundColor': '#003'}}>
+            <Card style={{'borderColor': cardColor}} className="coolerCard">
             {/* <Card.Img style={{'height': '7rem'}} variant="top" src="https://i.pinimg.com/474x/16/a8/3e/16a83e8583e19aacb560aae90c813e4a.jpg" /> */}
                 <Card.Body>
-                <Card.Title style={{'fontSize': '3rem', 'height': '7rem'}}>{deets.coolerName}</Card.Title>
+                <Card.Title className="coolerCardTitle">{deets.coolerName}</Card.Title>
                 <Card.Text>current pH: {ph || 'unavailable'}</Card.Text>
                 <Card.Text>location: {deets.location}</Card.Text>
 				</Card.Body>
 
-				<Card.Body style={{'padding': '0.7rem 0 0.7rem 0', 'width': '100%','display': 'flex', 'flexDirection': 'row', 'justifyContent': 'space-evenly'}}>
-					<Button onClick={() => updatepH('button')} style={{'width': '7rem','color': 'black', 'backgroundColor': '#EEF'}} variant="primary">update pH</Button>
-					<Button onClick={goToCoolerDetails} style={{'width': '7rem', 'color': 'black', 'backgroundColor': '#EEF'}}>all details</Button>
+				<Card.Body>
+					<Button onClick={() => updatepH('button')} className="coolerCardBtn" variant="primary">update pH</Button>
+					<Button onClick={goToCoolerDetails}className="coolerCardBtn">all details</Button>
 				{/* </div> */}
                 </Card.Body>
             </Card>
--- client/src/components/CoolerPage.js
+++ client/src/components/CoolerPage.js
@@ -1,48 +1,50 @@
 import axios from 'axios';
-import React, {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import SERVER_URL from '../utils/constants';
 import querystring from 'querystring';
-
+import '../styles/coolerPage.css';
 export default function CoolerPage() {
-    const {id} = useParams()
+    const { id } = useParams()
 
     const [cooler, setCooler] = useState({})
 
     useEffect(() => {
-        axios.post(`${SERVER_URL}/dashboard/getACooler`, querystring.stringify({id: id})
-        , {
-            headers: {
-          'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-        },
-        credentials: 'include',
-        withCredentials: true
-        }
-        )
-        .then(res => {
-            // console.log('cooler in frontend', res)    
-            if(res.status === 200){
-                console.log('got cooler in frontend', res);
-                setCooler(res.data)
+        axios.post(`${SERVER_URL}/dashboard/getACooler`, querystring.stringify({ id: id })
+            , {
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                },
+                credentials: 'include',
+                withCredentials: true
             }
-        })
-        .catch(err => console.log('error while getting cooler', err))
+        )
+            .then(res => {
+                // console.log('cooler in frontend', res)    
+                if (res.status === 200) {
+                    console.log('got cooler in frontend', res);
+                    setCooler(res.data)
+                }
+            })
+            .catch(err => console.log('error while getting cooler', err))
     }, [])
 
-    return(
-        <div  style={{'minHeight': '100vh','backgroundColor': '#004', 'color': 'white','display': 'flex', 'flexDirection': 'column','alignItems': 'center', 'paddingTop': '1rem'}}>
-           
-           <div class= "tile">
-           <h1>Cooler {cooler.coolerName}</h1>
-            {/* <p>name: {cooler.coolerName}</p> */}
-            <br></br>
-            <p>Location: {cooler.location}</p>
-            <p>Current pH: {cooler.currentpH}</p>
-            <p>Highest pH: {cooler.highestpH}</p>
-            <p>Description: very nice cooler</p>
+    return (
+        <div className="cooler-det">
+
+            <div class="tile">
+                <h1 className="cooler-page-para-val" style={{borderRadius:'10px'}}> &ensp; {cooler.coolerName}</h1>
+                {/* <p>name: {cooler.coolerName}</p> */}
+                <br></br>
+                <div className="coolerDetails">
+                    <p className="cooler-page-para">Location: <p className="cooler-page-para-val">{cooler.location}</p></p>
+                    <p className="cooler-page-para">Current pH: <p  className="cooler-page-para-val">{cooler.currentpH}</p></p>
+                    <p className="cooler-page-para">Highest pH: <p  className="cooler-page-para-val">{cooler.highestpH}</p></p>
+                    <p className="cooler-page-para">Description: <p className="cooler-page-para-val">very nice cooler</p> </p>
+                </div>
             </div>
         </div>
-        
+
     )
 }
